11eb8448e46b3f485388cd776602a69d70ea0f70
Victor Villazón <victorzn23@gmail.com>
2023-05-02 17:19:36 -0500

    feat(backend): hash password

--- backend/src/controllers/enterprises.controller.js
+++ backend/src/controllers/enterprises.controller.js
@@ -1,22 +1,36 @@
 import { Prisma } from "../libs/prisma.js"
+
 import slugify from 'slugify'
 import { addDays } from 'date-fns'
+import bcrypt from 'bcryptjs'
 
 const DEFAULT_CATEGORY = 'General'
 const DEFAULT_MEMBERSHIP_ROLE = 'OWNER'
+const PLAN_FREE = 'Free'
 
 export const registerEnterprise = async (req, res) => {
-  
   try {
     const { name, firstname, lastname, email, password, domain } = req.body
-  
-    const domainNormalized = slugify(domain)
     
-    const getPlanFree = await Prisma.plan.findFirst({ where: { name: 'Free' } })
+    const domainNormalized = slugify(domain)
 
-    const newEnterprise = await Prisma.enterprise.create({ data: { name, domain: domainNormalized } })
+    const newEnterprise = await Prisma.enterprise.create({
+      data: {
+        name,
+        domain: domainNormalized
+      }
+    })
 
-    const newUser = await Prisma.user.create({ data: { firstname, lastname, email, password } })
+    const passwordHashed = bcrypt.hashSync(password, 10);
+
+    const newUser = await Prisma.user.create({
+      data: {
+        firstname,
+        lastname,
+        email,
+        password: passwordHashed
+      }
+    })
 
     const newMembership = await Prisma.membership.create({
       data: {
@@ -26,7 +40,10 @@ export const registerEnterprise = async (req, res) => {
       }
     })
 
+    const getPlanFree = await Prisma.plan.findFirst({ where: { name: PLAN_FREE } })
+
     const currentDate = new Date()
+  
     const addDaysToCurrentDate = addDays(currentDate, getPlanFree.duration);
 
     const newSubscription = await Prisma.subscription.create({
@@ -36,22 +53,25 @@ export const registerEnterprise = async (req, res) => {
         trialDuration: getPlanFree.duration,
         planId: getPlanFree.id,
         enterpriseId: newEnterprise.id
-    }})
+      }
+    })
 
     const newCategory = await Prisma.category.create({
       data: { name: DEFAULT_CATEGORY, enterpriseId: newEnterprise.id }
     })
 
-    return res.status(201).json({ content: {
-      newEnterprise,
-      newUser,
-      newMembership,
-      newSubscription,
-      newCategory,
-      currentDate,
-      addDaysToCurrentDate,
-      getPlanFree
-    }, message: 'Empresa creada exitosamente' })
+    return res.status(201).json({
+      content: {
+        newEnterprise,
+        newUser,
+        newMembership,
+        newSubscription,
+        newCategory,
+        currentDate,
+        addDaysToCurrentDate,
+        getPlanFree
+      }, message: 'Empresa creada exitosamente'
+    })
   } catch (error) {
     console.log(error)
     return res.status(400).json({ message: 'Error al crear la empresa', content: error.message })
