dc730d31076b4ec5b501e3f2a3bae1e7202b1da9
Victor Villazón <victorzn23@gmail.com>
2023-05-02 14:35:45 -0500

    feat(backend): update plans seeds

--- backend/prisma/plansSeed.js
+++ backend/prisma/plansSeed.js
@@ -2,19 +2,19 @@ export const plans = [
   {
     name: 'Free',
     description: 'For hobbyists, students, and indie hackers.',
-    price: 5.00,
-    duration: 1
+    price: 0.00,
+    duration: 7
   },
   {
     name: 'Entrepreneur',
     description: 'For small entrepreneurs and early-stage startups.',
-    price: 10.00,
-    duration: 1
+    price: 5.00,
+    duration: 30
   },
   {
     name: 'Premium',
     description: 'For larger teams with complex needs.',
-    price: 20.00,
-    duration: 1
+    price: 10.00,
+    duration: 30
   }
 ]
