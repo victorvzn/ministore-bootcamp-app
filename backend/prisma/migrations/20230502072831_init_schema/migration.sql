7c82e24906cb44d1b14eaad67e22490128cff0eb
Victor Villazón <victorzn23@gmail.com>
2023-05-02 02:33:40 -0500

    feat(backend): add prisma migrations

---
+++ backend/prisma/migrations/20230502072831_init_schema/migration.sql
@@ -0,0 +1,188 @@
+-- CreateEnum
+CREATE TYPE "USER_ROLE" AS ENUM ('APP_ADMIN', 'APP_STAFF', 'ENTERPRISE_ADMIN', 'ENTERPRISE_STAFF', 'ENTERPRISE_CUSTOMER');
+
+-- CreateEnum
+CREATE TYPE "SUBSCRIPTION_STATUS" AS ENUM ('ACTIVED', 'ON_HOLD', 'CANCELED', 'AUTO_RENEWED');
+
+-- CreateEnum
+CREATE TYPE "MEMBERSHIP_ROLE" AS ENUM ('OWNER', 'MEMBER');
+
+-- CreateTable
+CREATE TABLE "enterprises" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "address" TEXT,
+    "phone_number" TEXT,
+    "domain" TEXT NOT NULL,
+    "active" BOOLEAN DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "enterprises_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
+    "address" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "USER_ROLE" NOT NULL DEFAULT 'ENTERPRISE_CUSTOMER',
+    "email_verified" BOOLEAN NOT NULL,
+    "active" BOOLEAN DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "memberships" (
+    "id" TEXT NOT NULL,
+    "role" "MEMBERSHIP_ROLE" NOT NULL,
+    "enterpriseId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "memberships_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sales_header" (
+    "id" TEXT NOT NULL,
+    "series" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "total" DOUBLE PRECISION NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+    "user_id" TEXT NOT NULL,
+    "enterprise_id" TEXT,
+
+    CONSTRAINT "sales_header_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sales_detail" (
+    "id" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "size" INTEGER NOT NULL,
+    "color" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+    "sales_header_id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "enterprise_id" TEXT,
+
+    CONSTRAINT "sales_detail_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "code" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "discount_percentage" DOUBLE PRECISION NOT NULL DEFAULT 0.00,
+    "stock" INTEGER NOT NULL DEFAULT 0,
+    "brand" TEXT,
+    "thumbnail" TEXT,
+    "images" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "sizes" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "colors" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "published" BOOLEAN DEFAULT false,
+    "active" BOOLEAN DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+    "category_id" TEXT NOT NULL,
+    "enterprise_id" TEXT,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "active" BOOLEAN DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+    "enterprise_id" TEXT,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subscriptions" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "trial_start" TIMESTAMP(3) NOT NULL,
+    "trial_end" TIMESTAMP(3) NOT NULL,
+    "subs_start" TIMESTAMP(3) NOT NULL,
+    "subs_end" TIMESTAMP(3) NOT NULL,
+    "duration" TEXT NOT NULL,
+    "status" "SUBSCRIPTION_STATUS" NOT NULL DEFAULT 'ON_HOLD',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "plan_id" TEXT NOT NULL,
+    "enterprise_id" TEXT NOT NULL,
+
+    CONSTRAINT "subscriptions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "plans" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "active" BOOLEAN DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+    "enterprise_id" TEXT,
+
+    CONSTRAINT "plans_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_name_price_category_id_key" ON "products"("name", "price", "category_id");
+
+-- AddForeignKey
+ALTER TABLE "memberships" ADD CONSTRAINT "memberships_enterpriseId_fkey" FOREIGN KEY ("enterpriseId") REFERENCES "enterprises"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "memberships" ADD CONSTRAINT "memberships_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sales_header" ADD CONSTRAINT "sales_header_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sales_detail" ADD CONSTRAINT "sales_detail_sales_header_id_fkey" FOREIGN KEY ("sales_header_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sales_detail" ADD CONSTRAINT "sales_detail_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_plan_id_fkey" FOREIGN KEY ("plan_id") REFERENCES "plans"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_enterprise_id_fkey" FOREIGN KEY ("enterprise_id") REFERENCES "enterprises"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
