9d0661613078af76a1f5eaa70876c4ee324e9e00
Victor Villazón <victorzn23@gmail.com>
2023-05-04 02:57:24 -0500

    feat(backend: enhance enterprices

--- backend/src/routes/enterprises.routers.js
+++ backend/src/routes/enterprises.routers.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import * as controllers from '../controllers/enterprises.controllers.js'
+import { validateToken } from '../middlewares/validateToken.js'
 
 export const enterprisesRouter = Router()
 
 enterprisesRouter.get(
   '/',
+  validateToken,
   controllers.listEnterprises
 )
