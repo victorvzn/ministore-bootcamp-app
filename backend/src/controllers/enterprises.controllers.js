40ac86d56ccda8566a31d422a85536501f92cf9d
Victor Villazón <victorzn23@gmail.com>
2023-05-04 02:48:46 -0500

    refactor(backend: validate enterprises by active and deteteAt

--- backend/src/controllers/enterprises.controllers.js
+++ backend/src/controllers/enterprises.controllers.js
@@ -2,11 +2,21 @@ import { Prisma } from "../libs/prisma.js"
 
 export const listEnterprises = async (req, res) => {
   try {
-    const enterprises = await Prisma.enterprise.findMany()
+    const userFound = req.user
+
+    const userEnterpriseId = userFound.enterprise.id
+
+    const enterprises = await Prisma.enterprise.findMany({
+      where: {
+        id: userEnterpriseId,
+        active: true,
+        deletedAt: null
+      }
+    })
 
     return res.status(201).json({ content: enterprises })
   } catch (error) {
     console.log(error)
-    return res.status(400).json({ message: 'Error al crear la empresa', content: error.message })
+    return res.status(400).json({ message: 'Error al lista las empresas', content: error.message })
   }
 }
