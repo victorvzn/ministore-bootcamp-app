c764f7e12b8f30e4fb24ac0936f9526273e3f3b2
Victor Villazón <victorzn23@gmail.com>
2023-05-03 21:07:52 -0500

    feat(backend): add validateDomain middleware

---
+++ backend/src/middlewares/validateDomain.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken'
+import { Prisma } from "../libs/prisma.js"
+
+export const validateDomain = async (req, res, next) => {
+  try {
+    const { domain } = req.query
+
+    if (!domain) throw new Error('Error validando domain')
+
+    const enterpriseFound = await Prisma.enterprise.findFirst({
+      where: { domain },
+      select: { id: true, name: true, domain: true }
+    })
+
+    const userEnterpriseFound = await Prisma.membership.findFirst({
+      where: { enterpriseId: enterpriseFound.id },
+      select: { id: true, role: true }
+    })
+
+    req.enterprise = {
+      ...enterpriseFound,
+      membership: userEnterpriseFound
+    }
+
+    next()
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({
+      message: 'Error',
+      content: error.message
+    })
+  }
+}
