706d6861760bca41162b167f83ddefae572bf0d3
Victor Villazón <victorzn23@gmail.com>
2023-05-04 14:02:26 -0500

    feat(frontend): change public products

--- frontend/src/app/api/v1/products/route.ts
+++ frontend/src/app/api/v1/products/route.ts
@@ -4,7 +4,7 @@ export async function GET(request: Request) {
   const url = new URL(request.url)
   console.log({ url })
 
-  const res = await fetch(`http://localhost:5000/api/v1/products/public?domain=${url.host}`)
+  const res = await fetch(`http://localhost:5000/api/v1/products/store?domain=${url.host}`)
   const data = await res.json()
 
   return NextResponse.json({ data })
