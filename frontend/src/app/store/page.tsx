9990495057dd169c03c4665ee1b529e031e44912
Victor Villazón <victorzn23@gmail.com>
2023-05-05 14:38:45 -0500

    feat(frontend): handle host origin

--- frontend/src/app/store/page.tsx
+++ frontend/src/app/store/page.tsx
@@ -1,11 +1,21 @@
+import { url } from 'inspector';
 import { Inter } from 'next/font/google'
+import { headers } from 'next/headers';
 
 const inter = Inter({ subsets: ['latin'] })
 
 const APP_URL = process.env.APP_URL_FRONTEND
 
-const fetchProducts = () => {
-  return fetch(`${APP_URL}/api/v1/products`)
+const fetchProducts = async () => {
+  const headersInstance = headers();
+
+  const host = await headersInstance.get('referer')
+
+  console.log({ host })
+
+  const url = new URL(host || '')
+
+  return fetch(`${url.origin}/api/v1/products`)
     .then(res => res.json())
     .catch(err => console.log(err))
 }
