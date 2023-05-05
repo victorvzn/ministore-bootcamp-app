af40b922c81867360323bbe70b0ae18b94fd6fab
Victor Villazón <victorzn23@gmail.com>
2023-05-05 11:33:57 -0500

    feat(frontend): change envs 2

---
+++ frontend/src/app/api/v1/products/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+
+const APP_CENTRAL_URL = process.env.APP_BACKEND_URL
+
+export async function GET(request: Request) {
+  const url = new URL(request.url)
+
+  const res = await fetch(`${APP_CENTRAL_URL}/api/v1/products/store?domain=${url.host}`)
+  const data = await res.json()
+
+  return NextResponse.json(data)
+}
